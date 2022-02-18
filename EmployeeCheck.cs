d29f60fea634d94ba1539c25ba7df36c99f7cf57
Chaitra <chaitra.p321@gmail.com>
2022-02-18 09:11:34 +0530

    Check Employee Present or Not

---
+++ EmployeeCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProgram
+{
+    public class EmployeeCheck
+    {
+        /// <summary>
+        /// UC1 - Employees the present or not.
+        /// </summary>
+        public static void EmployeePresentOrNot()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number == 1)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
