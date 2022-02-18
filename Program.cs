e2a220c48ef552c3ddb77078c690f1fb1fbec9cc
Chaitra <chaitra.p321@gmail.com>
2022-02-18 09:05:24 +0530

    Greeting Message for Employee Wage

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Program");
+            EmployeeCheck.EmployeePresentOrNot();
+        }
+    }
+}
