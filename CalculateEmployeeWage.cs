bf09cc72c9d878ff2e06785b7861078b664d91d1
Chaitra <chaitra.p321@gmail.com>
2022-02-18 09:28:50 +0530

    Calculate Wage

---
+++ CalculateEmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProgram
+{
+    public class CalculateEmployeeWage
+    {
+        /// <summary>
+        /// UC -2 Calculates the wage.
+        /// </summary>
+        public static void CalculateWage()
+        {
+            int WagePerHour = 20;
+            int FullWorkingDay = 8;
+            int calculateWage = 0;
+
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number == 1)
+            {
+                 calculateWage = WagePerHour * FullWorkingDay;
+
+                Console.WriteLine("Calculated Wage = " + calculateWage);
+
+            }
+            else
+            {
+                calculateWage = 0;
+                Console.WriteLine("Calculated Wage = " + calculateWage);
+            }
+        }
+    }
+}
